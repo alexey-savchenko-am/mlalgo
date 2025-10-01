01ad3fc98b5e3badf9e55369b7d5747cba85d166
Alexey Savchenko <alexey.repository@gmail.com>
2025-10-01 21:54:35 +0300

    Finish 2_train_text_classifier

--- tasks/2_train_text_classifier.py
+++ tasks/2_train_text_classifier.py
@@ -5,7 +5,8 @@ from collections import Counter
 from itertools import chain
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
-
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, confusion_matrix
 
 def main():
 
@@ -43,6 +44,19 @@ def main():
     X_train_tfidf = vectorizer.fit_transform(X_train)
     X_test_tfidf = vectorizer.transform(X_test)
 
+    #------------------- FIT THE MODEL --------------------------
+
+    model = LogisticRegression(max_iter=100000)
+    model.fit(X_train_tfidf, y_train)
+
+    y_pred = model.predict(X_test_tfidf)
+
+     #------------------- REPORT  --------------------------
+    print("Classification Report:\n")
+    print(classification_report(y_test, y_pred))
+
+    print("Confusion Matrix:\n")
+    print(confusion_matrix(y_test, y_pred))
 
 if __name__ == "__main__":
     main()
